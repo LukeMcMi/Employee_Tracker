a7daf06eafc54149b86f38d81655531e919b332e
Luke McMillan <88580305+LukeMcMi@users.noreply.github.com>
2022-01-25 21:30:11 +1100

    added initial question

---
+++ db/questions.js
@@ -0,0 +1,21 @@
+module.exports = {
+    initialQuestion: {
+        type: "list",
+        message: "What would you like to do?",
+        name: "initial",
+        choices: ["Add Employee",
+            "Add Department",
+            "Add Role",
+            "View Departments",
+            "View Employees",
+            "Update Employee's Role",
+            "View All Employees by Manager",
+            "Remove Employee",
+            "View All Employees by Ddepartment",
+            "View All Roles",
+            "Add a Role",
+            "Remove Roles",
+            "Exit"
+        ]
+    },
+}
