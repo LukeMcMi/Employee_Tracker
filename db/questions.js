c11e6caf9c9fe586dedfc7863fa8023d2afa6d25
Luke McMillan <88580305+LukeMcMi@users.noreply.github.com>
2022-01-25 21:38:53 +1100

    updated employee add

--- db/questions.js
+++ db/questions.js
@@ -22,5 +22,23 @@ module.exports = {
         type: "input",
         message: "What is your employee's first name?",
         name: "first_name",
+    },
+    {
+        type: "input",
+        message: "What is your employee's last name?",
+        name: "last_name",
+    },
+    {
+        type: "list",
+        message: "What is your employee's roleID?",
+        name: "role_id",
+        choices: roles
+    },
+    {
+        type: "list",
+        message: "Who is your employee's manager?",
+        name: "manager_id",
+        choices: employees
     }
+    ],
 }
