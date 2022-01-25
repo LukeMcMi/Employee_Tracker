72dbb60275a767df47de6a53c30ee055811c3aca
Luke McMillan <88580305+LukeMcMi@users.noreply.github.com>
2022-01-25 21:54:41 +1100

    added questions for remove role, quit and remove employee.

--- db/questions.js
+++ db/questions.js
@@ -20,47 +20,66 @@ module.exports = {
         ]
     },
     addEmployee: (roles, employees) => [{
-        type: "input",
-        message: "What is your employee's first name?",
-        name: "first_name",
-    },
-    {
-        type: "input",
-        message: "What is your employee's last name?",
-        name: "last_name",
-    },
-    {
-        type: "list",
-        message: "What is your employee's roleID?",
-        name: "role_id",
-        choices: roles
-    },
-    {
-        type: "list",
-        message: "Who is your employee's manager?",
-        name: "manager_id",
-        choices: employees
-    }
+            type: "input",
+            message: "What is your employee's first name?",
+            name: "first_name",
+        },
+        {
+            type: "input",
+            message: "What is your employee's last name?",
+            name: "last_name",
+        },
+        {
+            type: "list",
+            message: "What is your employee's roleID?",
+            name: "role_id",
+            choices: roles
+        },
+        {
+            type: "list",
+            message: "Who is your employee's manager?",
+            name: "manager_id",
+            choices: employees
+        }
     ],
+
     addDepartmentQuestions: {
         type: "input",
         message: "What is the name of your department?",
         name: "department_name",
     },
     addRole: [{
-        type: "input",
-        message: "What is the title of your new role?",
-        name: "titleRole",
+            type: "input",
+            message: "What is the title of your new role?",
+            name: "titleRole",
+        },
+        {
+            type: "input",
+            message: "What is the salary for this role?",
+            name: "salary",
+        },
+        {
+            type: "input",
+            message: "What is the department id for this role?",
+            name: "departmentIDrole",
+        }
+    ],
+    removeRole: {
+        type: "list",
+        message: "What is your employee's role?",
+        name: "roleRemoval",
+        choices: ["Manager", "Associate", "Soft"]
     },
-    {
-        type: "input",
-        message: "What is the salary for this role?",
-        name: "salary",
+    quit: {
+        type: "list",
+        message: "Are your sure you would like to quit?",
+        name: "quit",
+        choices: ["Yes", "No"]
     },
-    {
-        type: "input",
-        message: "What is the department id for this role?",
-        name: "departmentIDrole",
+    removeEmployee: {
+        type: "list",
+        message: "What is your employee's role?",
+        name: "employeeRemoval",
+        choices: ["Manager", "Associate", "Soft"]
     }
-],
 }
